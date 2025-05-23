cafeee9204ecc72dec7fae1587a311176753456f
Fernando Alvarado <fernandoalvarado@ciencias.unam.mx>
2025-05-22 22:43:50 -0600

    subiendo material apoyo

---
+++ ML/main.py
@@ -0,0 +1,6 @@
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+
+print("¡Todo está bien instalado!")
